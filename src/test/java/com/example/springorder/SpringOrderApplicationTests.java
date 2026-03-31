466cda69ef2ae195b0226943946a7dca226cb2e0
qkrwns1478 <qkrwns1478@gmail.com>
2026-03-31 15:56:42 +0900

    first commit

---
+++ src/test/java/com/example/springorder/SpringOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
