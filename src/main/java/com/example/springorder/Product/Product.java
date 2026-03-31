c0b929f7a7c329b3c842e75af06835cce48c1618
qkrwns1478 <qkrwns1478@gmail.com>
2026-03-31 17:43:12 +0900

    feat: 상품 CRUD 구현

---
+++ src/main/java/com/example/springorder/Product/Product.java
@@ -0,0 +1,30 @@
+package com.example.springorder.Product;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}
---
+++ src/main/java/com/example/springorder/Product/ProductController.java
@@ -0,0 +1,56 @@
+package com.example.springorder.Product;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    /* 상품 등록 */
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    /* 상품 목록 조회 */
+    @GetMapping
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    /* 상품 단건 조회 */
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    /* 상품 수정 */
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        Product new_product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        new_product.setName(product.getName());
+        new_product.setPrice(product.getPrice());
+        new_product.setQuantity(product.getQuantity());
+        return productRepository.save(new_product);
+    }
+
+    /* 상품 삭제 */
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        Product target_product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        productRepository.delete(target_product);
+    }
+}
