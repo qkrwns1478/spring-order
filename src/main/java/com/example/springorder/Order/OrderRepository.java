369e725d196bb2b8bee95edd661eb5b187dd9896
qkrwns1478 <qkrwns1478@gmail.com>
2026-04-02 17:52:06 +0900

    fix: N+1 문제 해결

--- src/main/java/com/example/springorder/Order/OrderRepository.java
+++ src/main/java/com/example/springorder/Order/OrderRepository.java
@@ -1,6 +1,11 @@
 package com.example.springorder.Order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @EntityGraph(attributePaths = "product")
+    Page<Order> findAll(Pageable pageable);
 }
